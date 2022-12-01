6f2081c73bdd784c4b94c3949a2dc3b7f3df2516
Vinay Rajaratnam <vjrajara@calpoly.edu>
2022-11-30 23:07:52 -0800

    started on face/eye detection

--- process.py
+++ process.py
@@ -13,28 +13,33 @@ tracker = Tracker()
 cap = cv2.VideoCapture(0)
 
 ret, frame = cap.read()
-
 rgb_out = tracker.detect_body(frame)
-
-
-
 
 while cap.isOpened():
     prev_frame = rgb_out[:]
     ret, frame = cap.read()
-    rgb_out = tracker.detect_body(frame)
 
     if not ret:
         print("Done...")
         break
 
+    # Body Outline
+    rgb_out = tracker.detect_body(frame)
     cv2.imshow('window', rgb_out)
 
+    # Body After-Effect
     rgb_out = addFeedback(rgb_out, prev_frame)
-
     prev_frame = rgb_out
-
     cv2.imshow('feedback', rgb_out)
+
+    # Eye Detection
+    # Problem: video contains silhouette of the performer, so face features aren't detected
+    # Possible Solutions:
+    #   detect circles (eyes) from the image produced from the edge detection feature
+    #   brighten the darker areas of the frame to detect face features
+
+    tracker.detect_face(frame)
+
     if cv2.waitKey(1) == 27:
         break
 
--- tracker.py
+++ tracker.py
@@ -16,3 +16,20 @@ class Tracker:
         # Apply Canny edge detection
         edges = cv2.Canny(image=blur, threshold1=10, threshold2=50)
         return edges
+
+    def detect_face(self, frame):
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        # profileface_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+        eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y , w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+
+        cv2.imshow('face/eyes', frame)
