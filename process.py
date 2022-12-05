abe47cb551a8a894b1ac69ed5a1854ac12b6f2ab
Vinay Rajaratnam <vjrajara@calpoly.edu>
2022-12-05 00:26:52 -0800

    detects facial features

--- process.py
+++ process.py
@@ -6,38 +6,35 @@ from tracker import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', default='./videos/simple pink dancing.mp4')
+parser.add_argument('--shape-predictor', help='path to facial landmark predictor',
+                    default='./shape_predictor_81_face_landmarks.dat')
 args = parser.parse_args()
 
-tracker = Tracker()
 # cap = cv2.VideoCapture(args.input)
 cap = cv2.VideoCapture(0)
 
 ret, frame = cap.read()
 
-rgb_out = tracker.detect_body(frame)
+rgb_out = detect_body(frame)
 
 r = 0
 prev_frame = np.zeros(frame.shape, dtype=int)
 
 kb.addHotkeys()
 
-
-#THE HEAD POSE FUNCTION ONLY WORKS ON A SINGLE IMAGE
-tracker.head_pose(frame)
+# THE HEAD POSE FUNCTION ONLY WORKS ON A SINGLE IMAGE
+head_pose(frame, args.shape_predictor)
 
 
 while cap.isOpened():
-
-
-
     ret, frame = cap.read()
-    rgb_out = tracker.detect_body(frame)
+    rgb_out = detect_body(frame)
 
     if not ret:
         print("Done...")
         break
 
-    cv2.imshow('window', rgb_out)
+    # cv2.imshow('window', rgb_out)
 
     channel = np.dstack([rgb_out, rgb_out, rgb_out])
 
@@ -50,7 +47,12 @@ while cap.isOpened():
 
     prev_frame = channel
 
-    cv2.imshow('feedback', channel)
+    # cv2.imshow('feedback', channel)
+
+    # HEAD POSE
+    head_pose(frame, args.shape_predictor)
+    cv2.imshow('head pose', frame)
+
     if cv2.waitKey(1) == 27:
         break
 
