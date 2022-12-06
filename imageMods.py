859bc7f867b62c707660247bbe50d0d82fbea09e
pjs507 <paulsoetaert@gmail.com>
2022-12-05 22:33:36 -0800

    final project

--- imageMods.py
+++ imageMods.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import keyboardChanges as kb
 
@@ -6,6 +8,7 @@ rval = 0
 gval = 0
 bval = 0
 colorIdx = 0
+colorCooldown = time.time()
 # def addFeedback(cur_frame, prev_frame):
 #     black = np.zeros(cur_frame.shape, dtype=int)
 #
@@ -26,29 +29,25 @@ def modifyChannel(channel, r=255, g=255, b=255):
 def headpoint_effects(xy_angles, channel):
     global feedbackAmount
     global colorIdx
+    global colorCooldown
+    cool = time.time()
     x = xy_angles[0]
     y = xy_angles[1]
     cutoff = 35
     if y >= cutoff:
-        print("L")
-
         if feedbackAmount > 0:
             feedbackAmount /= 2
 
     elif y <= -cutoff:
-        print("R")
-
         if feedbackAmount < 5:
             feedbackAmount += .5
 
     if x >= cutoff:
-        print("DOWN")
-
         channel = channel ^ -1
 
-    elif x <= -10:
-        print("UP")
+    elif x <= -10 and cool - colorCooldown > 2:
         colorIdx += 1
+        colorCooldown = time.time()
 
     channel[np.all(channel == (255, 255, 255), axis=-1)] = color_mode(colorIdx)
 
